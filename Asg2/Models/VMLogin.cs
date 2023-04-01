37507b4ae73f4e3786dee2255149ad993e524e8d
Laurentiu Tusa <lautusa@yahoo.com>
2023-04-02 02:47:48 +0300

    project start, login

---
+++ Asg2/Models/VMLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace Asg2.Models
+{
+    public class VMLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool KeepLoggedIn { get; set; }
+    }
+}
